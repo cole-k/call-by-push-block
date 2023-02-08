78856127105dad2a61bc24b10d80c8120b4c69fb
Cole K <cole-k@users.noreply.github.com>
2023-02-08 14:35:40 -0800

    push multiple things

--- 10-line-sokoban-golfed.hs
+++ 10-line-sokoban-golfed.hs
@@ -1,15 +1,12 @@
-x="λ..._ ..o.. ..o.. _█...\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+x="λλ.._ ..oo_ ..o.. _X...\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 y="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 z="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-main=g.o((,)<*>id<$>words).lines$x<>y<>z;b=reverse;o=map;a c=all(c`notElem`)
-g l|[]<-l=putStrLn"You win!"|(s,_):r<-l,all('_'`notElem`)s=g r
- |(s,u):r<-l=w s*>(q<$>getLine>>=(\i->g$(n i u([id,t.b,t,id,o b]!! i)s,u):r))
-m('λ':c:d)|c<'/'=".λ"<>m d|e:r<-d,c=='o',e<'o'=".λ"<>[last$'o':['█'|e>'.']]<>m r
+main=g.o((,)<*>id<$>words).lines$x%y%z;r=reverse;o=map;(%)=(<>);p=putStrLn;k="λ."
+g[]=p"You win!";g l|(s,_):v<-l,all('_'`notElem`)s=g v
+g((s,u):v)=mapM p s*>(q<$>getLine>>=(\i->g$(n i u([id,t.r,t,id,o r]!!i)s,u):v))
+m('λ':c:d)|c<'/'=r k%m d|(a,b)<-span(>'n')$c:d,a>[]=(r$take 1 b#r a)%m(drop 1 b)
 m"λ"=".";m(c:r)=c:m r;m l=l;e=[]:e;t=foldr(zipWith(:))e;q(c:_)=fromEnum c`mod`5
-n i u f|i<1=pure u|i<2=b.t.o m.f|0<3=f.o m.f;w=mapM putStrLn
-
+n i u f|i<1=pure u|i<2=r.t.o m.f|0<3=f.o m.f;""#(c:r)|c>'n'=r%k;"."#r=r%k
+"_"#('o':r)='X':r%k;x#l=x%l%"λ"
 -- ^ 10 ----------------------------------------------------------------------80
--- pushing multiple cursors and blocks: span(>'n') and handle the end case
--- using an auxilliary function. will require some thought on how to handle
--- walls since they are also >'n' (maybe change walls to 'X' or find characters
--- that work for the player and ball which are greater than the wall).
+-- Instructions: move: hjkl, reset: x
