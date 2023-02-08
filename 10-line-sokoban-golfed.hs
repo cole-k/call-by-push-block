eee429e4dc97620d9fa216e9472e70085dcd88b8
Cole K <cole-k@users.noreply.github.com>
2023-02-08 10:35:32 -0800

    add reset and golf a line

--- 10-line-sokoban-golfed.hs
+++ 10-line-sokoban-golfed.hs
@@ -1,10 +1,9 @@
-module Sokoban where
 x="λ..._ ..o.. ..o.. _█...\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 y="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 z="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 main=g.o((,)<*>id<$>words).lines$x<>y<>z;b=reverse;o=map;a c=all(c`notElem`)
-g l|[]<-l=putStrLn"You win!"|(s,_):r<-l,a 'o's=g r|(s,u):r<-l,a 'λ's=g$(u,u):r
- |(s,u):r<-l=w s*>(q<$>getLine>>=(\i->g$(n i([id,t.b,t,id,o b]!! i)s,u):r))
+g l|[]<-l=putStrLn"You win!"|(s,_):r<-l,all('_'`notElem`)s=g r
+ |(s,u):r<-l=w s*>(q<$>getLine>>=(\i->g$(n i u([id,t.b,t,id,o b]!! i)s,u):r))
 m('λ':c:d)|c<'/'=".λ"<>m d|e:r<-d,c=='o',e<'o'=".λ"<>[last$'o':['█'|e>'.']]<>m r
-m"λ"=".";m(c:r)=c:m r;m l=l;e=[]:e;t=foldr(zipWith(:))e
-n i f|i<1=id|i<2=b.t.o m.f|0<3=f.o m.f;w=mapM putStrLn;q(c:_)=fromEnum c`mod`5
+m"λ"=".";m(c:r)=c:m r;m l=l;e=[]:e;t=foldr(zipWith(:))e;q(c:_)=fromEnum c`mod`5
+n i u f|i<1=pure u|i<2=b.t.o m.f|0<3=f.o m.f;w=mapM putStrLn
