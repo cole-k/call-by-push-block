cdd1ba6181e6108f18e6a31afb8ce42d8bdebcb2
Cole K <cole-k@users.noreply.github.com>
2023-02-08 16:00:35 -0800

    WIP level design

--- 10-line-sokoban-golfed.hs
+++ 10-line-sokoban-golfed.hs
@@ -1,12 +1,13 @@
-x="λλ.._ ..oo_ ..o.. _X...\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-y="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+temp="Move:[hjkl] Reset:[x]\n.λ.o_ oX... .XXXX _..λ. _o.o_"
+x="Move:[hjkl] Reset:[x]\nλ.o._ _.o..\n..o_ λXoX oX_X _...\nX.._ λooo X.._\n.λ."
+y=".._ .o_o.. .o_.o_\n.λ.o_ oX... .XXXX _..λ. _o.o_\nxxxxxxxxxxxxxxxxxxxxxxxxxx"
 z="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-main=g.o((,)<*>id<$>words).lines$x%y%z;r=reverse;o=map;(%)=(<>);p=putStrLn;k="λ."
-g[]=p"You win!";g l|(s,_):v<-l,all('_'`notElem`)s=g v
-g((s,u):v)=mapM p s*>(q<$>getLine>>=(\i->g$(n i u([id,t.r,t,id,o r]!!i)s,u):v))
+main=g.o((,)<*>id<$>words).lines$ temp;r=reverse;(%)=(<>);p=putStrLn;k="λ."
+g[]=p"You win!";g l|(s,_):v<-l,all('_'`notElem`)s=j s*>g v
+g(w@(s,u):v)=j s*>(q<$>getLine>>=(\i->g$(i?([id,t,t.r,id,o r]!!i)$w,u):v))
 m('λ':c:d)|c<'/'=r k%m d|(a,b)<-span(>'n')$c:d,a>[]=(r$take 1 b#r a)%m(drop 1 b)
 m"λ"=".";m(c:r)=c:m r;m l=l;e=[]:e;t=foldr(zipWith(:))e;q(c:_)=fromEnum c`mod`5
-n i u f|i<1=pure u|i<2=r.t.o m.f|0<3=f.o m.f;""#(c:r)|c>'n'=r%k;"."#r=r%k
-"_"#('o':r)='X':r%k;x#l=x%l%"λ"
+i?f|i<1=snd|i==2=r.t.o m.f.fst|0<3=f.o m.f.fst;""#(c:r)|c>'n'=r%k;"."#r=r%k
+"_"#('o':r)='O':r%k;x#l=x%l%"λ";o=map;j=mapM p
 -- ^ 10 ----------------------------------------------------------------------80
 -- Instructions: move: hjkl, reset: x
