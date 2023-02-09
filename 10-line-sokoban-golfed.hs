1b5dadbcd24871ac9aea365d81aef99e36650b4e
Cole K <cole-k@users.noreply.github.com>
2023-02-08 22:32:22 -0800

    levels through 7

--- 10-line-sokoban-golfed.hs
+++ 10-line-sokoban-golfed.hs
@@ -1,9 +1,8 @@
-temp="Move:[hjkl] Reset:[x]\n.λ.o_ oX... .XXXX _..λ. _o.o_"
-x="Move:[hjkl] Reset:[x]\nλ.o._ _.o..\n..o_ λXoX oX_X _...\nX.._ λooo X.._\n.λ."
-y=".._ .o_o.. .o_.o_\n.λ.o_ oX... .XXXX _..λ. _o.o_\nxxxxxxxxxxxxxxxxxxxxxxxxxx"
-z="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-main=g.o((,)<*>id<$>words).lines$ temp;r=reverse;(%)=(<>);p=putStrLn;k="λ."
-g[]=p"You win!";g l|(s,_):v<-l,all('_'`notElem`)s=j s*>g v
+x="Move:[hjkl] Reset:[x]\nλ.o._ _.o..\nλ.o_ oXoX _X_X\nX._.. λoooo X..._\n.λ."
+y=".._ .o_o.. .o_.o_\n.λ.o_ oX... .XXXX _..λ. _o.o_\n._.o.. ._.o.X .o_.λ. .o_XX"
+z="X ....o_ .λX.o_\noλooo.... ooλoo.... ooλλ_____ oλooλ____ λooooλ... ........."
+main=g.o((,)<*>id<$>words).lines$x%y%z;r=reverse;(%)=(<>);p=putStrLn;k="λ."
+g[]=p"You win!";g l|(s,_):v<-l,all('_'`notElem`)s=j s*>p"Level start"*>g v
 g(w@(s,u):v)=j s*>(q<$>getLine>>=(\i->g$(i?([id,t,t.r,id,o r]!!i)$w,u):v))
 m('λ':c:d)|c<'/'=r k%m d|(a,b)<-span(>'n')$c:d,a>[]=(r$take 1 b#r a)%m(drop 1 b)
 m"λ"=".";m(c:r)=c:m r;m l=l;e=[]:e;t=foldr(zipWith(:))e;q(c:_)=fromEnum c`mod`5
