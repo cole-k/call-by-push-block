0042a0ef14d3d4aedc105baabb6646ecc84f4c82
Cole K <cole-k@users.noreply.github.com>
2023-02-13 12:33:08 -0800

    compress as much as possible

--- call-by-push-block.hs
+++ call-by-push-block.hs
@@ -1,37 +1,20 @@
 import System.IO (stdin, hSetBuffering, BufferMode(..))
 -- x="oλooo.... ooλoo.... ooλλ_____ oλooλ____ λooooλ... .........\nλoo+_\nλoλ+_\n##..... λλoo++_ ##.....\n"
-x="λoo+_\nλoλ+_"
 -- y="#.#.####### .λ.λ.###### .#.#.###### .....++++o_ #.#.####### .....###### .λλλ.######\n"
 -- z="...._.... ..._+.... ..#_+.... .#λ#+###. .#oooooλ# .#o#λ#o#. .#o###o#. #λoooo.#. .#####λ#. ......#.."
+x="⣡䜖㡼㫲⪵⥽㎤峛甤䤌✲䴣✓㡧孲㖭伡擡䤫学䢵㖡亜䣢㢦㦬桢媜䤵䲜✩㙟䤣ឞ䤛ᛴ⒑㋲⒋㙲䡛䤴䤤㙼㏐庁䤥㉴搃䤭䯤孥歭䌌孭䬵孥䵭त☀䬮孭䦭嬤歭䉌孭䤽䤔䤴䤂头䤂᬴孅㬴ᛛ㬵坍㬴坭㍴壛嬴卭䤴䬤<"
 main=do
-  hSetBuffering stdin NoBuffering
-  (f=<<).mapM g.zip[1..].o(pure.words).lines$x --o c x>>=o(i!!).n 5.w
-g(k,x@(s:_))|all('_'`notElem`)s=l x<$k!x<*p"⮑  "<*d
-g(k,x)=do{k!x;p b;i<-q.pure<$>d;g(k,i?([t.r,t,id,t,t,t,o r]!!i)$x)}
-m('λ':c:d)|(a,b)<-span(>'n')$c:d=r(u b#r a)%m(e b);m"λ"=".";m(c:r)=c:m r;m l=l;
-(1?f)x=u$r x;(5?f)x=e x%u(r x)%u(r x);(i?f)x=((i&f).o m.f.head$x):x;
-0&f=r.t;_&f=f
-d=getChar;
-q(x:_)=c x`mod`7;q _=0;
-r=reverse;
-v=[]:v;
-t=foldr(zipWith(:))v
-k!x@(s:_)=p"\^[cLvl "*>a k*>p"Steps: "*>a(l x)*>p(unlines s)
-""#(_:r)=r%k;"."#r=r%k;"+"#(_:r)='.':r%k;"+"#_="..";"_"#('o':r)='O':r%k;x#l=x%l%"λ"
-k="λ.";
-i="λ_.o+# \n";
-n=take;
-c=fromEnum;
-w n=n`mod`8:w(n`div`8);
-p=putStr;
-o=map
-(%)=(++);
-a=print;
-l=length;
-e=drop 1;
-u=take 1;
-f s=p"🎉\nSteps:"*>print s
-b="Move[wasd] Reset[x] Undo[u]\n"
+     hSetBuffering stdin NoBuffering
+     (f=<<).mapM g.zip[1..].o(pure.words).lines$o c x>>=o(i!!).n 5.w
+;v=[]:v;k="λ."
+b="λ:wasd 🔄:x 🔙:u\n";g(k,x)|all('_'`notElem`)$head x=l x<$k!x<*p"⮑  "<*d
+g(k,x)=do{k!x;p b;i<-q<$>d;g(k,i?([t.r,t,id,t,t,t,o r]!!i)$x)};n=take;c=fromEnum
+m('λ':c:d)|(a,b)<-span(>'n')$c:d=r(u b#r a)%m(e b);m"λ"=".";m(c:r)=c:m r;m l=l
+(1?f)x=u$r x;(5?f)x=e x%u(r x)%u(r x);(i?f)x=((i&f).o m.f.head$x):x;0&f=r.t;_&f=f
+p=putStr;""#(_:r)=r%k;"."#r=r%k;"+"#(_:r)='.':r%k;"+"#_="..";"_"#('o':r)='O':r%k
+x#l=x%l%"λ";k!x@(s:_)=p"\^[cL"*>a k*>p"Step "*>a(l x)*>p(unlines s);a=print
+q(x:_)=c x`mod`7;q _=0;d=getLine;i="+λ_o.# \n";r=reverse;e=drop 1;u=take 1;o=map
+f s=p"🎉"*>print s;t=foldr(zipWith(:))v;(%)=(++);l=length;w n=n`mod`8:w(n`div`8)
 --wasdxu [0,6,3,2,1,5]
 -- ^10 ------------------------------------------------------------------ 80> --
 {- gam-10-80-hs-prelude/call-by-push-block (cole-k), ghc 9.2.5
