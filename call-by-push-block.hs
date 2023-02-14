f9d2227cab0d73f5508bddac729da50ffb7fd49b
Cole K <cole-k@users.noreply.github.com>
2023-02-13 20:33:08 -0800

    I was wrong: 19*5=95 tiles now.
    Also added a devmode command to skip levels. Last level encoding
    is broken and it's probably my fault for counting wrong _or_ an
    issue with leading 0s.

--- call-by-push-block.hs
+++ call-by-push-block.hs
@@ -1,13 +1,13 @@
-x="⣡䜖㡼㫲⪵⥽㎤峛甤䤌✲䴣✓㡧孲㖭伡擡䤫学䢵㖡亜䣢㢦㦬桢媜䤵䲜✩㙟䤣ឞ䤛ᛴ⒑㋲⒋㙲䡛䤴䤤㙼㏐庁䤥㉴搃䤭䯤孥歭䌌孭䬵孥䵭त☀䬮孭䦭嬤歭䉌孭䤽䤔䤴䤂头䤂᬴孅"
+x="㗐⺝琢緫㳮㖦煒毿䚒⒂㺫⪗㺟琔涫睶⢐埐Ⓑ涕⓮睐⣺◓瓕畲吓泺⒮⫺㺰縼⒗燽⒗ǽ㛛總㛘翅⒇⒕⒒ῄร洙⒒縅櫉⒖斢液⮶搐涶斕液╶ኒ埉斖涶⒮涒⮶搀涶⒔⒓㒕⒑㲕⒑憕ⶱ"
 main=print=<<(mapM g.zip[1..].o(pure.words).lines$o(i!!).n 5.w=<<o c(x%y));o=map
 t=foldr(zipWith(:))v;e=drop 1;g(k,x)|all(notElem '_')$head x=l x<$k!x<*p"⮑  "<*d
 g(k,x)=do{k!x;p b;i<-q<$>d;g(k,i?([t.r,t,id,t,t,t,o r]!!i)$x)};c=fromEnum;k="λ."
 m('λ':c:d)|(a,b)<-span(>'n')$c:d=r(n 1 b#r a)%m(e b);m"λ"=".";m(c:r)=c:m r;m l=l
-(1?f)x=z x;(5?f)x=e x%z x%z x;(i?f)x=((i&f).o m.f.head$x):x;0&f=r.t;_&f=f;n=take
+(1?f)x=z x;(5?f)x=e x%z x%z x;(4?f)_=[[]];(i?f)x=((i&f).o m.f.head$x):x;0&f=r.t;_&f=f;n=take
 p=putStr;""#(_:r)=r%k;"."#r=r%k;"+"#(_:r)='.':r%k;"+"#_="..";"_"#('o':r)='O':r%k
-x#l=x%l%"λ";k!x@(s:_)=p"\^[cL"*>a k*>a(l x)*>p(unlines s);i="+λ_o.# \n";l=length
-q(x:_)=c x`mod`7;q _=0;d=getLine;(%)=(++);r=reverse;b="λ:[wasd] 🔄:[x] 🔙:[u]\n"
-a=print;v=[]:v;z=n 1.r;w n=n`mod`8:w(n`div`8);y="㬴ᛛ㬵坍㬴坭㍴壛嬴卭䤴䬤<"
+x#l=x%l%"λ";k!x@(s:_)=p"\^[cL"*>a k*>a(l x)*>p(unlines s);i="λ+._\n #o";l=length
+q(x:_)=c x`mod`7;q _=0;d=getLine;v=[]:v;z=n 1.r;r=reverse;b="λ:wasd 🔄:x 🔙:u\n"
+a=print;(%)=(++);w n=n`mod`8:w(n`div`8);y="羕懿澕ⷰ澕ⷶ縵ⲿ涕ⰶ⒕▒$"
 -- ^10 ------------------------------------------------------------------ 80> --
 {- gam-10-80-hs-prelude/call-by-push-block (cole-k), ghc 9.2.5
 
