e21cbe3fc838439acf9765bdd669bff4aa255edc
Cole K <cole-k@users.noreply.github.com>
2023-02-17 10:47:35 -0800

    final version?

--- call-by-push-block.hs
+++ call-by-push-block.hs
@@ -1,5 +1,5 @@
 import System.IO
-x="⠥䄖ॼز⚳⣻த㰀甤䤬Ⅎ䴠ℐ१㛲֛伥搥䤘㛦䢳֥亄䠢ࢦজ楢㚄䤳䲄ℝŇ䤠ਆ䤠娆⒒ņ⒕.䤨䤦䤤ု兺㟑䤤Ů㲉䤣㣼ⓜ奦怜䜜䱋䤤䞤ᛣ䤱ä学璜䤜䤤䤴䤣䴤䣥䤤㛞㣛垛ሀ摉㛛"
+x="杘㫲㛋䚴գᛜ㞜孂㜕ٳⳛ㮫櫓㎫ⵘ䤤ㅲニ䪵㓛⤤マ坲㛎⃜㵛㚝㛠䦢㔴㜘㫐⛮㣛ਜ਼㝭媛㝨媛涀શ氭傶ㅭ㚛㛛偛ⷝ瘨㤎⛛筀䢷㛛㣡ⶤッ㕬䜜㚿㛛䜓缣㛚⭣〃㎴㛜㛛䛚㛛ڛ㛣⛛䤤䣤"
 main=do
      hSetBuffering stdin NoBuffering
      print=<<(mapM g.zip[1..].o(pure.words).lines$o(i!!).n 5.w=<<o c(x%y))
@@ -11,8 +11,8 @@ m('λ':c)|(a,b)<-span(>'n')c=r(n 1 b#r a)%m(e b);m(c:r)=c:m r;m l=l;k="λ.";n=ta
 (1?f)x=z x;(5?f)x=e x%z x%z x;(i?f)x=(i&f$o m$f$head x):x;0&f=r.t;_&f=f;p=putStr
 d=getChar;(%)=(++);""#(_:r)=r%k;""#""=".";"."#r=r%k;"+"#(_:r)='.':r%k;"+"#_=".."
 "_"#('o':r)='O':r%k;x#l=x%l%"λ";q(x:_)=c x`mod`7;q _=0;r=reverse;z=n 1.r;a=print
-k!x@(s:_)=p"\^[cL"*>a k*>a(l x)*>p(unlines s);i="o+_#.λ \n";b="λ:wasd 🔄:x 🔙:u\n"
-l=length;w n=n`mod`8:w(n`div`8);y="䛣䤳㤤㴤䥤䥣䤧䤢⤦䤡✦䤡㫦䛙æ㨀ユ䘝ユ䘛Ş䜀㛦䝛䤦䣤ᤧ䤊䡦䌡䤦䤤ܞ㣃䬦䥤㣦䜜ᄞ㠊䤞㤤翿"
+k!x@(s:_)=p"\^[cL"*>a k*>a(l x)*>p(unlines s);i="λ\n .#o_+";b="λ:wasd 🔄:x 🔙:u\n"
+l=length;w n=n`mod`8:w(n`div`8);y="儔翭䖿䤤䜣㛚㣛㢛䛃ᛃ㞛⛳瞛⛳砣✤季Ⱝॣ✬䥣✬孆✝䤣✄㛛ᛳ滛⛟盻⟛㛛⛛䬜⣥㘛⛃㣣✜潜⣯㛜ᣛ"
 -- ^10 ------------------------------------------------------------------ 80> --
 {- gam-10-80-hs-prelude/call-by-push-block (cole-k), ghc 9.2.5
 
